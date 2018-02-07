f659bec8d4588b2bcffd3004efefabc4e43ea7e3
2DAWManelRiera <daw2mrieratorrent@iesjoanramis.org>
2018-02-07 16:09:18 +0100

    Primer commit

---
+++ llenguatgesClient/holajs.js
@@ -0,0 +1,16 @@
+<!DOCTYPE hmtl>
+<html lang="ca">
+
+	<head>
+		<title>Hola, Ramis!</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="hola.css" />
+	</head>
+	<body>
+		<button type="button"
+		onclick='document.getElementById('demo').innerHTML = "Hola, Ramis!"'>
+		Click me</button>
+		
+		<p id="demo"></p>
+	</body>
+</html>
---
+++ llenguatgesServidor/hola.jsp
@@ -0,0 +1,17 @@
+<html>
+<head><title>Hola, Ramis!</title></head>
+<body>
+  <%
+    double num = Math.random();
+    if (num > 0.95) {
+  %>
+      <h2>Hola, Ramis!</p>
+  <%
+    } else {
+  %>
+      <h2>Uep, Ramis!</h2>
+  <%
+    }
+  %>
+</body>
+</html>
